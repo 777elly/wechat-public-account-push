35394f27bed43f8914be44f228bab087c7187bb2
777elly <114902014+777elly@users.noreply.github.com>
2022-10-03 15:25:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'honey',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_kZn67jsuOLTUyA_ueeIsD4yBHQ',
+      id: 'o_kZn6wNN-FDtMPQdzQwopn3QrRk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'sN2hWR_jYJuRzTsz0pjbvwnAUi8AVkiv8dd2-LSmVGg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
